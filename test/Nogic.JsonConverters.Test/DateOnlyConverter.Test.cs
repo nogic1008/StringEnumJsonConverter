854dab76cd2af79d9aca9e58536495796c8f637a
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-01-17 17:34:50 +0900

    fix: nullable assertion

--- test/Nogic.JsonConverters.Test/DateOnlyConverter.Test.cs
+++ test/Nogic.JsonConverters.Test/DateOnlyConverter.Test.cs
@@ -12,14 +12,14 @@ public class DateOnlyConverterTest
         => new() { Converters = { new DateOnlyConverter(format) } };
 
     [Theory]
-    [InlineData(null, "\"0001-01-01\"", 1, 1, 1)]
+    [InlineData("yyyy-MM-dd", "\"0001-01-01\"", 1, 1, 1)]
     [InlineData("yy/MM/dd", "\"22/01/01\"", 2022, 1, 1)]
     [InlineData("yyyyMMdd", "\"99991231\"", 9999, 12, 31)]
     public void CanDeserializeJson(string? format, string json, int year, int month, int day)
         => JsonSerializer.Deserialize<DateOnly>(json, CreateOption(format)).Should().Be(new(year, month, day));
 
     [Theory]
-    [InlineData(null, "\"2020/01/01\"")]
+    [InlineData("yyyy-MM-dd", "\"2020/01/01\"")]
     [InlineData("yyyy/MM/dd", "\"2023/10/10 0:00:00.000+09:00\"")]
     [InlineData("yy/MM/dd", "\"200101\"")]
     public void CannotDeserializeJson(string? format, string json)
@@ -33,7 +33,7 @@ public class DateOnlyConverterTest
 
     private static IEnumerable<object?[]> CanSerializeJsonData() => new[]
     {
-        new object?[] { null, DateOnly.MinValue, "\"0001-01-01\"" },
+        new object?[] { "yyyy-MM-dd", DateOnly.MinValue, "\"0001-01-01\"" },
         new object?[] { "yy/MM/dd", new DateOnly(2022, 1, 1), "\"22/01/01\"" },
         new object?[] { "yyyy/MM/dd", DateOnly.MaxValue, "\"9999/12/31\"" },
     };

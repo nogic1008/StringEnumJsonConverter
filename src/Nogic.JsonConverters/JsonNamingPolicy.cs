e2a1134f2a53374a53af88231a1ba719d90ab8c7
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-01-19 17:19:41 +0900

    feat: add `JsonNamingPolicy` for `snake_case` & `kebab-case` (#6)
    - `JsonLowerSnakeCaseNamingPolicy` for `lower_snake_case`
    - `JsonUpperSnakeCaseNamingPolicy` for `UPPER_SNAKE_CASE`
    - `JsonKebabCaseNamingPolicy` for `kebab-case`

---
+++ src/Nogic.JsonConverters/JsonNamingPolicy.cs
@@ -0,0 +1,125 @@
+namespace Nogic.JsonConverters;
+
+/// <summary>Helper for <see cref="JsonNamingPolicy"/></summary>
+public abstract class JsonNamingPolicyBase : JsonNamingPolicy
+{
+    /// <summary>
+    /// <inheritdoc cref="JsonNamingPolicyBase(char)" path="/param[@name='separator']"/>
+    /// </summary>
+    private readonly char _separator;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonNamingPolicyBase"/>.
+    /// </summary>
+    /// <param name="separator">Word Separator</param>
+    protected JsonNamingPolicyBase(char separator) => _separator = separator;
+
+    /// <summary>
+    /// Returns need to insert separator or not.
+    /// </summary>
+    /// <param name="c">original char</param>
+    protected virtual bool IsWordSeparator(char c)
+        => char.IsUpper(c) || char.IsDigit(c) || IsSkipWrite(c);
+
+    /// <summary>
+    /// Returns need to write <paramref name="c"/> or not.
+    /// </summary>
+    /// <param name="c">original char</param>
+    protected virtual bool IsSkipWrite(char c)
+        => "-_".Contains(c) || char.IsWhiteSpace(c);
+
+    /// <summary>
+    /// Convert <paramref name="c"/> for write <see langword="char[]"/>.
+    /// Used in <see cref="ConvertName"/>.
+    /// </summary>
+    /// <param name="isTopOfWord">Cursor is top of word or not</param>
+    /// <param name="c">original char</param>
+    protected abstract char ConvertForWrite(bool isTopOfWord, char c);
+
+    /// <inheritdoc/>
+    public sealed override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return name;
+
+        int length = 0;
+        bool isTopOfWord = true;
+        for (int i = 0; i < name.Length; i++)
+        {
+            char c = name[i];
+            if (IsWordSeparator(c))
+            {
+                if (!isTopOfWord)
+                {
+                    isTopOfWord = true;
+                    length++;
+                }
+            }
+            else
+            {
+                isTopOfWord = false;
+            }
+            if (!IsSkipWrite(c))
+                length++;
+        }
+        char[] buf = new char[length];
+
+        isTopOfWord = true;
+        int written = 0;
+        for (int i = 0; i < name.Length; i++)
+        {
+            char c = name[i];
+            if (IsWordSeparator(c))
+            {
+                if (!isTopOfWord)
+                {
+                    isTopOfWord = true;
+                    buf[written++] = _separator;
+                }
+            }
+            else
+            {
+                isTopOfWord = false;
+            }
+            if (!IsSkipWrite(c))
+                buf[written++] = ConvertForWrite(isTopOfWord, c);
+        }
+        return new string(buf);
+    }
+}
+
+/// <summary>Naming policy for lower_snake_casing.</summary>
+public sealed class JsonLowerSnakeCaseNamingPolicy : JsonNamingPolicyBase
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonLowerSnakeCaseNamingPolicy"/>
+    /// </summary>
+    public JsonLowerSnakeCaseNamingPolicy() : base('_') { }
+
+    /// <inheritdoc/>
+    protected override char ConvertForWrite(bool _, char c) => char.ToLowerInvariant(c);
+}
+
+/// <summary>Naming policy for UPPER_SNAKE_CASING.</summary>
+public sealed class JsonUpperSnakeCaseNamingPolicy : JsonNamingPolicyBase
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonUpperSnakeCaseNamingPolicy"/>
+    /// </summary>
+    public JsonUpperSnakeCaseNamingPolicy() : base('_') { }
+
+    /// <inheritdoc/>
+    protected override char ConvertForWrite(bool _, char c) => char.ToUpperInvariant(c);
+}
+
+/// <summary>Naming policy for kebab-casing.</summary>
+public sealed class JsonKebabCaseNamingPolicy : JsonNamingPolicyBase
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="JsonKebabCaseNamingPolicy"/>
+    /// </summary>
+    public JsonKebabCaseNamingPolicy() : base('-') { }
+
+    /// <inheritdoc/>
+    protected override char ConvertForWrite(bool _, char c) => char.ToLowerInvariant(c);
+}
---
+++ test/Nogic.JsonConverters.Test/JsonNamingPolicy.Test.cs
@@ -0,0 +1,56 @@
+namespace Nogic.JsonConverters.Test;
+
+/// <summary>Unit test for <see cref="JsonNamingPolicyBase"/></summary>
+public class JsonLowerCaseNamingPolicyTest
+{
+    /// <summary>Unit test for <see cref="JsonLowerSnakeCaseNamingPolicy"/></summary>
+    /// <param name="name">Original name</param>
+    /// <param name="expected">Converted name</param>
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("PascalCase", "pascal_case")]
+    [InlineData("camelCase", "camel_case")]
+    [InlineData("snake_case", "snake_case")]
+    [InlineData("kebab-case", "kebab_case")]
+    [InlineData("word  word", "word_word")]
+    [InlineData("UPPER", "upper")]
+    [InlineData("lower", "lower")]
+    [InlineData("camelUPPER", "camel_upper")]
+    [InlineData("WithNumber123", "with_number_123")]
+    public void JsonLowerSnakeCaseNamingPolicy_ConvertName(string name, string expected)
+        => new JsonLowerSnakeCaseNamingPolicy().ConvertName(name).Should().Be(expected);
+
+    /// <summary>Unit test for <see cref="JsonUpperSnakeCaseNamingPolicy"/></summary>
+    /// <param name="name">Original name</param>
+    /// <param name="expected">Converted name</param>
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("PascalCase", "PASCAL_CASE")]
+    [InlineData("camelCase", "CAMEL_CASE")]
+    [InlineData("snake_case", "SNAKE_CASE")]
+    [InlineData("kebab-case", "KEBAB_CASE")]
+    [InlineData("word  word", "WORD_WORD")]
+    [InlineData("UPPER", "UPPER")]
+    [InlineData("lower", "LOWER")]
+    [InlineData("camelUPPER", "CAMEL_UPPER")]
+    [InlineData("WithNumber123", "WITH_NUMBER_123")]
+    public void JsonUpperSnakeCaseNamingPolicy_ConvertName(string name, string expected)
+        => new JsonUpperSnakeCaseNamingPolicy().ConvertName(name).Should().Be(expected);
+
+    /// <summary>Unit test for <see cref="JsonKebabCaseNamingPolicy"/></summary>
+    /// <param name="name">Original name</param>
+    /// <param name="expected">Converted name</param>
+    [Theory]
+    [InlineData("", "")]
+    [InlineData("PascalCase", "pascal-case")]
+    [InlineData("camelCase", "camel-case")]
+    [InlineData("snake_case", "snake-case")]
+    [InlineData("kebab-case", "kebab-case")]
+    [InlineData("word  word", "word-word")]
+    [InlineData("UPPER", "upper")]
+    [InlineData("lower", "lower")]
+    [InlineData("camelUPPER", "camel-upper")]
+    [InlineData("WithNumber123", "with-number-123")]
+    public void JsonKebabCaseNamingPolicy_ConvertName(string name, string expected)
+        => new JsonKebabCaseNamingPolicy().ConvertName(name).Should().Be(expected);
+}

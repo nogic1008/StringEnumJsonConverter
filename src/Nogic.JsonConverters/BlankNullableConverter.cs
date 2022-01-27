e172737304f8b2ab98d728b649bb3b6dac12a0e5
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-01-27 11:23:59 +0900

    feat: create `BlankNullableConverter<T>`

---
+++ src/Nogic.JsonConverters/BlankNullableConverter.cs
@@ -0,0 +1,33 @@
+namespace Nogic.JsonConverters;
+
+/// <summary>Json converter for <see cref="Nullable{T}"/> that treats "" as <see langword="null"/>.</summary>
+/// <typeparam name="T">struct value type</typeparam>
+public class BlankNullableConverter<T> : JsonConverter<T?> where T : struct
+{
+    /// <summary>
+    /// <inheritdoc cref="BlankNullableConverter(JsonConverter{T})" path="/param[@name='converter']"/>
+    /// </summary>
+    private readonly JsonConverter<T> _converter;
+
+    /// <summary>Initializes a new instance of <see cref="BlankNullableConverter{T}"/>.</summary>
+    /// <param name="converter">
+    /// Json converter for <typeparamref name="T"/> that uses non-null value.
+    /// </param>
+    public BlankNullableConverter(JsonConverter<T> converter) => _converter = converter;
+
+    /// <inheritdoc/>
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+        reader.TokenType != JsonTokenType.Null
+        && (reader.TokenType != JsonTokenType.String || !string.IsNullOrEmpty(reader.GetString()))
+            ? _converter.Read(ref reader, typeof(T), options)
+            : null;
+
+    /// <inheritdoc/>
+    public override void Write(Utf8JsonWriter writer, T? value, JsonSerializerOptions options)
+    {
+        if (!value.HasValue)
+            writer.WriteNullValue();
+        else
+            _converter.Write(writer, value.Value, options);
+    }
+}
